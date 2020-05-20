a8df46c2768e7b4dd7b1a535fbee7a193e91e7dc
“InceptionV5” <88439872@qq.com>
2020-05-20 14:35:30 +0800

    添加voc2coo代码，imageid问题待处理

--- voc2coco.py
+++ voc2coco.py
@@ -1,92 +1,78 @@
-import sys
 import os
+import glob
 import json
+import shutil
+import numpy as np
 import xml.etree.ElementTree as ET
 
+path2 = "."
 
 START_BOUNDING_BOX_ID = 1
-PRE_DEFINE_CATEGORIES = {}
-# If necessary, pre-define category and its id
-#  PRE_DEFINE_CATEGORIES = {"aeroplane": 1, "bicycle": 2, "bird": 3, "boat": 4,
-                         #  "bottle":5, "bus": 6, "car": 7, "cat": 8, "chair": 9,
-                         #  "cow": 10, "diningtable": 11, "dog": 12, "horse": 13,
-                         #  "motorbike": 14, "person": 15, "pottedplant": 16,
-                         #  "sheep": 17, "sofa": 18, "train": 19, "tvmonitor": 20}
 
 
 def get(root, name):
-    vars = root.findall(name)
-    return vars
+    return root.findall(name)
 
 
 def get_and_check(root, name, length):
     vars = root.findall(name)
     if len(vars) == 0:
-        raise NotImplementedError('Can not find %s in %s.'%(name, root.tag))
+        raise NotImplementedError('Can not find %s in %s.' % (name, root.tag))
     if length > 0 and len(vars) != length:
-        raise NotImplementedError('The size of %s is supposed to be %d, but is %d.'%(name, length, len(vars)))
+        raise NotImplementedError('The size of %s is supposed to be %d, but is %d.' % (name, length, len(vars)))
     if length == 1:
         vars = vars[0]
     return vars
 
 
-def get_filename_as_int(filename):
-    try:
-        filename = os.path.splitext(filename)[0]
-        return int(filename)
-    except:
-        raise NotImplementedError('Filename %s is supposed to be an integer.'%(filename))
-
-
-def convert(xml_list, xml_dir, json_file):
-    list_fp = open(xml_list, 'r')
-    json_dict = {"images": [], "type": "instances", "annotations": [],
-                 "categories": []}
-    categories = PRE_DEFINE_CATEGORIES
+def convert(xml_list, json_file):
+    json_dict = {"images": [], "type": "instances", "annotations": [], "categories": []}
+    categories = pre_define_categories.copy()
     bnd_id = START_BOUNDING_BOX_ID
-    for line in list_fp:
-        line = line.strip()
-        print("Processing %s" % (line))
-        xml_f = os.path.join(xml_dir, line)
+    all_categories = {}
+    for index, line in enumerate(xml_list):
+        # print("Processing %s"%(line))
+        xml_f = line
         tree = ET.parse(xml_f)
         root = tree.getroot()
-        path = get(root, 'path')
-        if len(path) == 1:
-            filename = os.path.basename(path[0].text)
-        elif len(path) == 0:
-            filename = get_and_check(root, 'filename', 1).text
-        else:
-            raise NotImplementedError('%d paths found in %s'%(len(path), line))
 
-        # The filename must be a number
-        image_id = get_filename_as_int(filename)
+        filename = os.path.basename(xml_f)[:-4] + ".jpg"
+        image_id = 20200000001 + index
         size = get_and_check(root, 'size', 1)
         width = int(get_and_check(size, 'width', 1).text)
         height = int(get_and_check(size, 'height', 1).text)
-        image = {'file_name': filename, 'height': height, 'width': width,
-                 'id':image_id}
+        image = {'file_name': filename, 'height': height, 'width': width, 'id': image_id}
         json_dict['images'].append(image)
 
         # Cruuently we do not support segmentation
-        # segmented = get_and_check(root, 'segmented', 1).text
-        # assert segmented == '0'
+        #  segmented = get_and_check(root, 'segmented', 1).text
+        #  assert segmented == '0'
         for obj in get(root, 'object'):
             category = get_and_check(obj, 'name', 1).text
+            if category in all_categories:
+                all_categories[category] += 1
+            else:
+                all_categories[category] = 1
             if category not in categories:
-                new_id = len(categories)
+                if only_care_pre_define_categories:
+                    continue
+                new_id = len(categories) + 1
+                print(
+                    "[warning] category '{}' not in 'pre_define_categories'({}), create new id: {} automatically".format(
+                        category, pre_define_categories, new_id))
                 categories[category] = new_id
             category_id = categories[category]
             bndbox = get_and_check(obj, 'bndbox', 1)
-            xmin = int(get_and_check(bndbox, 'xmin', 1).text) - 1
-            ymin = int(get_and_check(bndbox, 'ymin', 1).text) - 1
-            xmax = int(get_and_check(bndbox, 'xmax', 1).text)
-            ymax = int(get_and_check(bndbox, 'ymax', 1).text)
-            assert(xmax > xmin)
-            assert(ymax > ymin)
+            xmin = int(float(get_and_check(bndbox, 'xmin', 1).text))
+            ymin = int(float(get_and_check(bndbox, 'ymin', 1).text))
+            xmax = int(float(get_and_check(bndbox, 'xmax', 1).text))
+            ymax = int(float(get_and_check(bndbox, 'ymax', 1).text))
+            assert (xmax > xmin), "xmax <= xmin, {}".format(line)
+            assert (ymax > ymin), "ymax <= ymin, {}".format(line)
             o_width = abs(xmax - xmin)
             o_height = abs(ymax - ymin)
-            ann = {'area': o_width*o_height, 'iscrowd': 0, 'image_id':
-                   image_id, 'bbox': [xmin, ymin, o_width, o_height],
+            ann = {'area': o_width * o_height, 'iscrowd': 0, 'image_id':
+                image_id, 'bbox': [xmin, ymin, o_width, o_height],
                    'category_id': category_id, 'id': bnd_id, 'ignore': 0,
                    'segmentation': []}
             json_dict['annotations'].append(ann)
@@ -99,10 +85,34 @@ def convert(xml_list, xml_dir, json_file):
     json_str = json.dumps(json_dict)
     json_fp.write(json_str)
     json_fp.close()
-    list_fp.close()
+    print("------------create {} done--------------".format(json_file))
+    print("find {} categories: {} -->>> your pre_define_categories {}: {}".format(len(all_categories),
+                                                                                  all_categories.keys(),
+                                                                                  len(pre_define_categories),
+                                                                                  pre_define_categories.keys()))
+    print("category: id --> {}".format(categories))
+    print(categories.keys())
+    print(categories.values())
 
 
 if __name__ == '__main__':
+    classes = ['chimney', 'smoke']
 
+    pre_define_categories = {}
+    for i, cls in enumerate(classes):
+        pre_define_categories[cls] = i + 1
+    # pre_define_categories = {'a1': 1, 'a3': 2, 'a6': 3, 'a9': 4, "a10": 5}
+    only_care_pre_define_categories = True
+    # only_care_pre_define_categories = False
 
-    convert(sys.argv[1], sys.argv[2], sys.argv[3])
+    sub_dir = 'train'
+    img_path = 'E://污染源识别/all-jpg/chimney/' + sub_dir + '/'
+    json_file = 'E://污染源识别/all-jpg/chimney/annotations/instances_' + sub_dir + '.json'
+
+    img_list = glob.glob(img_path+'*.jpg')
+    xml_list = []
+    for img in img_list:
+        file = img[img.rfind('\\')+1:-4] + '.xml'
+        xml_list.append('E://污染源识别/all-jpg/chimney/outputs/' + file)
+
+    convert(xml_list, json_file)
